ae60981194f036319204d2f1ad4bbdee36799c48
rockmanck <rockman.roll@gmail.com>
2014-11-11 18:54:47 +0200

    test commit
    want to know how to commit

--- app/src/androidTest/java/com/geekhub/lakhmaniuk/rssreader/ApplicationTest.java
+++ app/src/androidTest/java/com/geekhub/lakhmaniuk/rssreader/ApplicationTest.java
@@ -9,5 +9,6 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+		assert 2==2; // to test how to commit into git
     }
 }
